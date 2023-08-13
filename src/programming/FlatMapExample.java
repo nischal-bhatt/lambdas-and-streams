e22de4d4c1dd37f4ad1dda89f01cb6c34c8d41ac
Nishy baby <nischal.bhatt.87@gmail.com>
2023-08-13 18:40:11 +0800

    asd

--- src/programming/FlatMapExample.java
+++ src/programming/FlatMapExample.java
@@ -1,5 +1,6 @@
 package programming;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +22,19 @@ public class FlatMapExample {
                 .map(course -> course.split(""))
                 .flatMap(Arrays::stream)
                 .forEach(System.out::println);
+
+        List<List<Integer>> integers
+                = new ArrayList<>();
+        integers.add(List.of(2,3,5));
+        integers.add(List.of(1,11,9));
+        integers.add(List.of(2,2,22));
+
+        List<Integer> collect1 = integers
+                .stream()
+                .flatMap(list -> list.stream())
+                .collect(Collectors.toList());
+
+        System.out.println(collect1);
 
     }
 }
